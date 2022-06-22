78ef73a2412c46f2fa1f45ae9eb5e7cf542835bd
Jordan Hobson <hobsoncjordan@gmail.com>
2022-06-21 21:34:00 -0500

    Pushing with good output but no random pic

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 'use strict'
 // Array to hold all product objects
+let allPossibleImages = ['bag.jpg','banana.jpg','bathroom.jpg','boots.jpg','breakfast.jpg','bubblegum.jpg','chair.jpg','cthulu.jpg','dog-duck.jpg','dragon.jpg','pen.jpg','pet-sweep.jpg','scissors.jpg','shark.jpg','tauntaun.jpg','unicorn.jpg','water-can.jpg','wine-glass.jpg'];
 let allProducts = [];
 // Array to hold all product names
-let productNames = ['bag', 'banana', 'bathroom', 'boots'];
+let productNames = ['bag', 'banana', 'bathroom', 'boots', 'dragon'];
 let maxClicks = 25;
 let totalClicks = 0;
 // Create a constructor function that creates an object associated with 
@@ -25,7 +26,7 @@ function Product(name,path){
 //images directory and display them side-by-side-by-side in the browser window.
 function getRandomImage(){
     //Math.random gives me a whole number
-    return Math.floor(Math.random()*productNames.length);
+    console.log(Math.floor(Math.random()*allPossibleImages.length));
 }
 //Getting elements from html
 const imageContainer = document.getElementById('image-container');
@@ -34,23 +35,41 @@ let resultsButton = document.getElementById('results-button')
 //Making an image for every name in the product name array
 let img_one = document.querySelector('#image-container img:first-child');
 let img_two = document.querySelector('#image-container img:nth-child(2)');
+let img_three = document.querySelector('#image-container img:nth-child(3)');
+let img_four = document.querySelector('#image-container img:nth-child(4)');
+let img_five = document.querySelector('#image-container img:nth-child(5)');
 
 // Instance variables --> Objects
 let bag = new Product('bag','./assets/bag.jpg');
 let banana = new Product('banana','./assets/banana.jpg');
+let bathroom = new Product('bathroom','./assets/bathroom.jpg');
+let boots = new Product('boots','./assets/boots.jpg');
+let dragon = new Product('dragon','./assets/dragon.jpg');
 
 function constructImages(){
     //let bag0 = new Product(productNames[0],'./assets'+productNames[0]+'.jpg');
     //Adding path to image / Adding path attribute
     img_one.setAttribute('src',bag.path);
-    img_two.setAttribute('src',banana.path);
     img_one.setAttribute('alt',bag.name);
+    img_two.setAttribute('src',banana.path);
     img_two.setAttribute('alt',banana.name);
+    img_three.setAttribute('src',bathroom.path);
+    img_three.setAttribute('alt',bathroom.name);
+    img_four.setAttribute('src',boots.path);
+    img_four.setAttribute('alt',boots.name);
+    img_five.setAttribute('src',dragon.path);
+    img_five.setAttribute('alt',dragon.name);
 
     img_one.addEventListener('click',function(){trackClicks(bag)});
     img_two.addEventListener('click',function(){trackClicks(banana)});
+    img_three.addEventListener('click',function(){trackClicks(bathroom)});
+    img_four.addEventListener('click',function(){trackClicks(boots)});
+    img_five.addEventListener('click',function(){trackClicks(dragon)});
     timesShown(bag);
     timesShown(banana);
+    timesShown(bathroom);
+    timesShown(boots);
+    timesShown(dragon);
     
 }
 
@@ -72,6 +91,21 @@ function timesShown(product){
         product.timesShown++;
         console.log(product.timesShown);
     }
+    else if (product.name === img_three.alt){
+        console.log(`The ${product.name} is on the page.`);
+        product.timesShown++;
+        console.log(product.timesShown);
+    }
+    else if (product.name === img_four.alt){
+        console.log(`The ${product.name} is on the page.`);
+        product.timesShown++;
+        console.log(product.timesShown);
+    }
+    else if (product.name === img_five.alt){
+        console.log(`The ${product.name} is on the page.`);
+        product.timesShown++;
+        console.log(product.timesShown);
+    }
     else{
         console.log('there is no image here');
     }
@@ -80,7 +114,7 @@ function timesShown(product){
 function trackClicks(product){
     // Incrementing the times clicked if the times clicked
     // is less than the max clicks
-    if (product.timesClicked < maxClicks){
+    if (totalClicks < maxClicks){
         product.timesClicked++;
         totalClicks++;
         console.log(product.timesClicked);
@@ -106,9 +140,7 @@ function displayResults(productArray){
     }
 }
 function showResults(){
-    if (totalClicks===maxClicks){
-        displayResults(allProducts);
-    }
+    displayResults(allProducts);
 }
 
 constructImages(allProducts);
